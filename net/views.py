09b1171f2554bfefa6f9d872a261150cad234c23
Benard18 <benkaranja43@gmail.com>
2018-07-19 19:54:10 +0300

    placing the popular api and calling the result list

--- net/views.py
+++ net/views.py
@@ -4,3 +4,9 @@ import requests
 
 
 # Create your views here.
+
+
+def index(request):
+	popular = requests.get('https://api.themoviedb.org/3/movie/popular?api_key=c2ebfc2b45b369bc6c954edff69fad57')
+	populardata = popular.json()
+	datapopular = populardata['results']
