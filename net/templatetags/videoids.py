a213a4c4af72ff52650273ab2be6e72cafc4798e
Benard18 <benkaranja43@gmail.com>
2018-07-19 19:58:49 +0300

    adding the piping based on the youtube api

---
+++ net/templatetags/videoids.py
@@ -0,0 +1,19 @@
+from django import template
+import requests
+register=template.Library()
+
+@register.filter(name='datas')
+def data(value):
+	response = requests.get(f'https://www.googleapis.com/youtube/v3/search?part=snippet&order=viewCount&q={value}%2DOfficial+&type=video&videoCaption=closedCaption&key=AIzaSyCUJSh3QGecsOPq-orMsEiLn5muL1sotKQ')	
+	datas = response.json()
+	print(datas.keys())
+	try:
+		iddatas = datas['items'][0]['id']['videoId']
+	except:
+		return 'sdddfdssxxx'	
+	print(iddatas)
+	return iddatas
+
+@register.filter(name='genreids')
+def getter(a):
+	return {28:"Action",12:"Adventure",16:"Adventure",35:"Comedy",80:"Crime",99:"Documentary",18:"Drama",10751:"Family",14:"Fantasy",36:"History",27:"Horror",10402:"Music", 9648: "Mystery",878:"Science Fiction",10770: "TV Movie", 53: "Thriller",10752: "War",37: "Western"}.get(a)
