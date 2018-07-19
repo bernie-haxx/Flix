0a727b35a946e3883cfc1f1203ad249545830c75
Benard18 <benkaranja43@gmail.com>
2018-07-19 19:55:09 +0300

    placing the now playing data and doing the necessary of rendering the template

--- net/views.py
+++ net/views.py
@@ -13,3 +13,12 @@ def index(request):
 	upcoming = requests.get('https://api.themoviedb.org/3/movie/upcoming?api_key=c2ebfc2b45b369bc6c954edff69fad57')
 	upcomingdata = upcoming.json()
 	dataupcoming = upcomingdata['results']
+	nowplaying = requests.get('https://api.themoviedb.org/3/movie/now_playing?api_key=c2ebfc2b45b369bc6c954edff69fad57')
+	nowplayingdata = nowplaying.json()
+	datanowplaying = nowplayingdata['results']
+
+	return render(request,'index.html',{
+		'popular' : datapopular,
+		'upcoming':dataupcoming,
+		'now_playing':datanowplaying
+	})
